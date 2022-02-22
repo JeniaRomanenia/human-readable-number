d4a6cd341a659e49ddaa9bfe5adedcdb5757d9a5
Jenia Romanenia <71782573+JeniaRomanenia@users.noreply.github.com>
2022-02-22 19:14:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,25 @@
 module.exports = function toReadable (number) {
-    const nums = [
-        '', 'one', 'two', 'three', 'four',
-        'five', 'six', 'seven', 'eight', 'nine',
-        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
-        'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
-    ];
+    let result;
+    const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+        num10 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+        num20 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+        num100 = ['hundred', 'thousand', 'million', 'billion'],
+        n = String(num).split('');
 
-    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
-    let string = '';
-    
-    if(number == 0){
-        string = "zero";
-    }
-    else if(number < 20)
-    {
-       string = ones[number];
-    }
-    else if(number < 100) {
-        string = tens[Math.floor(number / 10)] + " " + nums[number % 10];
-    }
-    else {
-        tensNumber = number % 100;
-        string = nums[Math.floor(number/ 100)] + " hundred ";
-        if(tensNumber < 20)
-        {
-            string += nums[tensNumber];
+    if (num < 10) {
+        result = num[n[0]];
+    } else if (num >= 10 && num < 20) {
+        result = num10[n[1]];
+    } else if (num >= 20 && num < 100) {
+        result = `${num20[+n[0] - 2]} ${n[1] == 0 ? '' : num[n[1]]}`;
+    } else if (num >= 100 && num < 1000) {
+        result = `${num[n[0]]} ${num100[0]}`;
+        if (num % 100 > 9 && num % 100 < 20) {
+            result += ` ${num10[+n[2]]}`;
+        } else {
+            result += ` ${n[1] == 0 ? '' : num20[+n[1] - 2]} ${n[2] == 0 ? '' :num[n[2]]}`;
         }
-        else
-        {
-            string += tens[Math.floor(tensNumber / 10)] + " " + ones[tensNumber % 10];
-        }
-    }
 
-    return string.trim();
-}
+    }
+    return result.trim();
+};
